86832e799763a037c57484c00da3b9b50d700811
alexeses <46347629+alexeses@users.noreply.github.com>
2023-02-01 13:24:33 +0100

    login changes auth

--- app/app/src/main/java/org/fastchat/screens/LoginActivity.java
+++ app/app/src/main/java/org/fastchat/screens/LoginActivity.java
@@ -1,6 +1,7 @@
 package org.fastchat.screens;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,10 +9,22 @@ import org.fastchat.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    LinearLayout llLoginData;
+    LinearLayout llRegisterData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        llLoginData = findViewById(R.id.llLoginData);
+        llRegisterData = findViewById(R.id.llRegisterData);
+
+        llLoginData.setVisibility(LinearLayout.VISIBLE);
+        llRegisterData.setVisibility(LinearLayout.GONE);
+
+
 
 
     }
